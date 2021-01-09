6f206282cb66525a0932239a7509cc5694deef2e
HassenH1 <hasansaid51@gmail.com>
2021-01-09 14:02:47 -0800

    chapter 1 finished

--- chapter 1/stringRotation.js
+++ chapter 1/stringRotation.js
@@ -1,5 +1,16 @@
 //assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one calll to isSubstring
 
-function isSubstring(s1, s2) {}
+function isSubstring(A, B) {
+  if (A.length !== B.length) return false;
+  if ((A.length || B.length) === 0) return true;
 
-isSubstring("waterbottle", "erbottlewat");
+  for (var i = 0; i < A.length; i++) {
+    const reversed = A.slice(i).concat(A.slice(0, i));
+
+    if (reversed === B) return true;
+  }
+
+  return false;
+}
+
+console.log(isSubstring("waterbottle", "erbottlewat"));
