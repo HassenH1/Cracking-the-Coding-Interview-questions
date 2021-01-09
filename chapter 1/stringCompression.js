ad7efa0cae2f57f0f463d59a4fe24186f7ae7e75
HassenH1 <hasansaid51@gmail.com>
2021-01-08 18:28:02 -0800

    1.6

---
+++ chapter 1/stringCompression.js
@@ -0,0 +1,1 @@
+//implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. if the "compressed" string would not become smaller than the original string, your method should return the original string. you can assume the string has only uppercase and lowercase letters
