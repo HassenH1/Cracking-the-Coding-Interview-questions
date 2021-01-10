ca1f8cb6bf02318764c2ce2d57f5387cf3f366e3
HassenH1 <hasansaid51@gmail.com>
2021-01-09 18:25:41 -0800

    removeDup finished

--- chapter 2/removeDup.js
+++ chapter 2/removeDup.js
@@ -32,13 +32,33 @@ class Linkedlist {
     this.tail.next = newNode;
     this.tail = newNode;
   }
-}
+  /////////////////////////
+  removeDup() {
+    let hash = {};
 
-function removeDup(ray) {
-  let l = new Linkedlist();
-  for (let i = 0; i < ray.length; i++) {
-    l.append(array[i]);
+    let p1 = this.head;
+    let p2 = p1.next;
+    hash[p1.val] = true;
+
+    while (p2) {
+      let val = p2.val;
+      if (hash[val]) {
+        p1.next = p2.next;
+      } else {
+        hash[val] = true;
+        p1 = p2;
+      }
+      p2 = p2.next;
+    }
   }
+  /////////////////////////
 }
+// {12: true, 11: true, }
+//               prev n
 let array = [12, 11, 12, 21, 41, 43, 21];
-console.log(removeDup(array)); //12->11->21->41->43
+let l = new Linkedlist();
+for (let i = 0; i < array.length; i++) {
+  l.append(array[i]);
+}
+l.removeDup();
+console.log(l.trav()); //12->11->21->41->43
