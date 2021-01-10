fbd319a888299fcf237b96dcad40a9a7a79bf139
HassenH1 <hasansaid51@gmail.com>
2021-01-10 14:17:01 -0800

    sumLists linked list

---
+++ chapter 2/sumLists.js
@@ -0,0 +1,1 @@
+//you have two numbers represented by a linked list, where each node contains a single digit. the digits are sorted in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list
