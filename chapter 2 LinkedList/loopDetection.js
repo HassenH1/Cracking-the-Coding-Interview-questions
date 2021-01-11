f2d75a302ef1f7d996301e70a63969ea0968267c
HassenH1 <hasansaid51@gmail.com>
2021-01-10 19:45:28 -0800

    loop detection

---
+++ chapter 2 LinkedList/loopDetection.js
@@ -0,0 +1,6 @@
+//Given a circular linked list, implement an algo that returns the node at the beginning of the loop
+//DEFINITION
+//Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, sp as to make a loop in the linked list
+//Example
+//input a - b - c - d - e - c [the same c as eariler]
+//output : c
