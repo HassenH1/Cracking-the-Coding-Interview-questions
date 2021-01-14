b41ceeb537066f15f5d0ee1c1ee01ed030cba3e3
HassenH1 <hasansaid51@gmail.com>
2021-01-13 19:33:14 -0800

    ..

---
+++ chapter 4 Trees and graphs/minimalTree.js
@@ -0,0 +1,3 @@
+//Given a sorted (increasing order) array with unique integer elements, write an algo to create a binary search tree with minimal height
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
