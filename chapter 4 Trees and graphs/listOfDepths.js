a210c260fb4b7c3eae21ca32a1137fad7a2f6f51
HassenH1 <hasansaid51@gmail.com>
2021-01-14 11:07:01 -0800

    list of depths UNFINISHED

--- chapter 4 Trees and graphs/listOfDepths.js
+++ chapter 4 Trees and graphs/listOfDepths.js
@@ -26,7 +26,7 @@ class BinarySearchTree {
         } else {
           node.left = newNode;
         }
-      } else if (node.right < val) {
+      } else if (node.val < val) {
         if (node.right) {
           insert(node.right);
         } else {
@@ -37,4 +37,49 @@ class BinarySearchTree {
 
     insert(this.root);
   }
+  createLinkedList(node = this.root) {
+    class LinkedNode {
+      constructor(val) {
+        this.val = val;
+        this.next = null;
+      }
+    }
+
+    class LinkedList {
+      constructor() {
+        this.head = this.tail = null;
+      }
+      insert(val) {
+        let newNode = new LinkedNode(val);
+        if (this.head === null) {
+          this.head = newNode;
+          this.tail = this.head;
+        }
+        this.tail.next = newNode;
+        this.tail = this.tail.next;
+      }
+      trav() {
+        let node = this.head;
+        while (node !== null) {
+          console.log(node.val, "<--------the val?");
+          node = node.next;
+        }
+      }
+    }
+    let l = new LinkedList();
+
+    if (node) {
+      this.createLinkedList(node.left);
+      l.insert(node.val);
+      this.createLinkedList(node.right);
+    }
+    console.log(l, "<---the l?");
+  }
 }
+let b = new BinarySearchTree();
+let array = [8, 3, 10, 1, 6, 14, 4, 7, 13];
+for (let i of array) {
+  b.append(i);
+}
+// console.log(b);
+b.createLinkedList();
