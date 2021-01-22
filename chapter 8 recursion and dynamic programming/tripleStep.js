faaaed9c18c3cbc1be32e7d602c7987631b3152d
HassenH1 <hasansaid51@gmail.com>
2021-01-21 17:38:57 -0800

    triple step solution

--- chapter 8 recursion and dynamic programming/tripleStep.js
+++ chapter 8 recursion and dynamic programming/tripleStep.js
@@ -1,14 +1,28 @@
 //a child is running up a staircase with n steps and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method to count how many possible ways that child can run up the stairs
 
 /**
- *                   3
+ *                                3
  *
- *             -1   -2   -3
+ *                          -1   -2   -3
+ *
+ *                     2          1          0
+ *
+ *            -1 -2 -3        -1 -2 -3         -1 -2 -3
+ *
+ *     1    0    -1         0    -1    -2          X
+ *
+ *
  *
  */
 
 function staircase(num) {
-  console.log(num);
+  if (num < 0) {
+    return 0;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return staircase(num - 1) + staircase(num - 2) + staircase(num - 3);
+  }
 }
 
 console.log(staircase(3));
