f1b84dcf2b8754217fec65bc84b2583df87b7236
HassenH1 <hasansaid51@gmail.com>
2021-01-22 19:12:25 -0800

    permutation

---
+++ chapter 8 recursion and dynamic programming/permutationsWithoutDups.js
@@ -0,0 +1,5 @@
+//write a method to compute all permutations of a string of unique characters
+
+function permutationsWithoutDups(str) {}
+let string = "abcd";
+console.log(permutationsWithoutDups(string));
