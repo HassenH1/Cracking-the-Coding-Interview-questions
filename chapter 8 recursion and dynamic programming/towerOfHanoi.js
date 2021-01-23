14dff10c1a098f3d5fefb780d8696710bdbef6a2
HassenH1 <hasansaid51@gmail.com>
2021-01-22 18:37:54 -0800

    ..

---
+++ chapter 8 recursion and dynamic programming/towerOfHanoi.js
@@ -0,0 +1,9 @@
+//in the classic problem of the towers of hanoi, you have 3 towers and N disks of different sizes which can slide onto any tower. The puzzles starts with disks sorted in ascending order of size from top to bottom (i.e., each disk sits on top of an even larger one). You have the following constraints:
+// 1) Only one disk can be moved at a time
+// 2) A disk is slid off the top of one tower onto another tower
+// 3) A disk cannot be placed on top of a smaller disk
+// write a program to move the disks from the first tower to the last using stacks
+
+/**
+ *
+ */
