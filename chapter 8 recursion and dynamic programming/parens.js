b6788dd6f95251a58f015bf373441445afa0072f
HassenH1 <hasansaid51@gmail.com>
2021-01-23 11:44:26 -0800

    parens

---
+++ chapter 8 recursion and dynamic programming/parens.js
@@ -0,0 +1,4 @@
+//implement an algo to print all valud (e.g., proply opened and closed) combinations of n pairs of parentheses
+//EXAMPLE
+// input : 3
+// output: ((())), (()()), (())(), ()(()), ()()()
--- chapter 8 recursion and dynamic programming/permutationsWithoutDups.js
+++ chapter 8 recursion and dynamic programming/permutationsWithoutDups.js
@@ -1,5 +1,26 @@
 //write a method to compute all permutations of a string of unique characters
+/**
+ *
+ * [abcd, bcda, cdab, dabc, ]
+ *
+ */
 
-function permutationsWithoutDups(str) {}
+function permutationsWithoutDups(str) {
+  let ans = [];
+  function recursive(curr, remaining) {
+    if (remaining.length === 0) {
+      ans.push(curr);
+    } else {
+      for (let i = 0; i < remaining.length; i++) {
+        recursive(
+          curr + remaining.charAt(i),
+          remaining.slice(0, i) + remaining.slice(i + 1)
+        );
+      }
+    }
+  }
+  recursive("", str);
+  return ans;
+}
 let string = "abcd";
 console.log(permutationsWithoutDups(string));
