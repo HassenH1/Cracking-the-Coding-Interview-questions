804297d92e046dbf0d5390b1d1208be14f40e0e2
HassenH1 <hasansaid51@gmail.com>
2021-01-22 18:15:55 -0800

    recursive multiply

---
+++ chapter 8 recursion and dynamic programming/recursiveMultiply.js
@@ -0,0 +1,7 @@
+//write a recursive function to multiply two positive integers without using the *operator. You can use addition, subtraction and bit shifting, but you should minimize the number of those operations.
+
+function recursiveMethod(num) {
+  return num + num;
+}
+
+console.log(recursiveMethod(2));
