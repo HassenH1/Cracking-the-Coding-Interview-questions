2f2f0616d6bb5e202b139915b7db5404410e93f2
HassenH1 <hasansaid51@gmail.com>
2021-01-23 18:18:13 -0800

    wow

--- chapter 8 recursion and dynamic programming/parens.js
+++ chapter 8 recursion and dynamic programming/parens.js
@@ -2,3 +2,32 @@
 //EXAMPLE
 // input : 3
 // output: ((())), (()()), (())(), ()(()), ()()()
+
+var parens = function (n) {
+  var answers = [];
+  var recurse = function (currParens, remainingPairs) {
+    if (remainingPairs === 0) {
+      answers.push(currParens);
+    } else {
+      var used = {};
+      if (!used[`(${currParens})`]) {
+        used[`(${currParens})`] = true;
+        recurse(`(${currParens})`, remainingPairs - 1);
+      }
+      if (!used[`()${currParens}`]) {
+        used[`()${currParens}`] = true;
+        recurse(`()${currParens}`, remainingPairs - 1);
+      }
+      if (!used[`${currParens}()`]) {
+        used[`${currParens}()`] = true;
+        recurse(`${currParens}()`, remainingPairs - 1);
+      }
+    }
+  };
+  recurse("", n);
+  return answers;
+};
+
+/* TEST */
+var testn = 3;
+console.log(parens(testn));
