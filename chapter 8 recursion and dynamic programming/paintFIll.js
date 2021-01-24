dee12c6dc1279ca351619d072b76a0244f745efa
HassenH1 <hasansaid51@gmail.com>
2021-01-23 18:40:11 -0800

    wow dude

--- chapter 8 recursion and dynamic programming/paintFIll.js
+++ chapter 8 recursion and dynamic programming/paintFIll.js
@@ -1,5 +1,17 @@
 //Implement the "paint fill" function that one might see on many image eiditing programs. That is, given a screen (represented by a two-dimensopma; array of colors), a point, and a new color, fill in the surrounding area until the color changes from the original color
 
-function paintFill() {}
+function paintFill(screen, point, color) {}
+var b = "#000000";
+var w = "#ffffff";
+var g = "#00ff00";
 
-console.log(paintFill());
+var testScreen = [
+  [b, b, b, b],
+  [b, w, w, b],
+  [b, w, w, b],
+  [b, b, b, b],
+];
+
+var testPoint = [1, 1];
+
+console.log(paintFill(testScreen, testPoint, g));
