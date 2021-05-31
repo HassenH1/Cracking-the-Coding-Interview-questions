da38fb0e3857b502e9692321628c785cb2cba938
hassen <hasansaid51@gmail.com>
2021-05-30 17:01:04 -0700

    updated chapter 1 rotate matrix solution

--- chapter 1/rotateMatrix.js
+++ chapter 1/rotateMatrix.js
@@ -1,37 +1,68 @@
 //Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 
 function rotateMatrix(mat) {
-  let len = mat.length;
+	let len = mat.length;
 
-  for (let layer = 0; layer < Math.floor(len / 2); layer++) {
-    let first = layer;
-    let last = len - 1 - layer;
+	for (let layer = 0; layer < Math.floor(len / 2); layer++) {
+		let first = layer;
+		let last = len - 1 - layer;
 
-    for (let i = first; i < last; i++) {
-      let offset = i - first;
+		for (let i = first; i < last; i++) {
+			let offset = i - first;
 
-      let top = mat[first][i]; //save top
+			let top = mat[first][i]; //save top
 
-      //left -> top
-      mat[first][i] = mat[last - offset][first];
+			//left -> top
+			mat[first][i] = mat[last - offset][first];
 
-      //bottom -> left
-      mat[last - offset][first] = mat[last][last - offset];
+			//bottom -> left
+			mat[last - offset][first] = mat[last][last - offset];
 
-      //right -> bottom
-      mat[last][last - offset] = mat[i][last];
+			//right -> bottom
+			mat[last][last - offset] = mat[i][last];
 
-      //top -> right
-      mat[i][last] = top; //<---saved top
-    }
-  }
-  return mat;
+			//top -> right
+			mat[i][last] = top; //<---saved top
+		}
+	}
+	return mat;
 }
 
+//easier to understand 
+function rotate(mat){
+	for(let i = 0; i < mat.length; i++){
+		for(let j = i; j < mat.length; j++){
+			swap(mat, i, j, "diagonal")
+		}
+	}
+
+	for(let i = 0; i < mat.length; i++){
+		for(let j = 0; j < mat.length/2; j++){
+			swap(mat, i, j)
+		}
+	}
+	return mat;
+}
+
+function swap(mat, i, j, type = ""){
+	let len = mat.length
+		if(type === "diagonal"){
+			let temp = mat[i][j]
+				mat[i][j] = mat[j][i]
+				mat[j][i] = temp
+		} else {
+			let temp = mat[i][j] 
+				mat[i][j] = mat[i][len - 1 - j] 
+				mat[i][len - 1 - j] = temp 
+		}
+}
+
+
 let matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
 ];
 
-console.log(rotateMatrix(matrix));
+//console.log(rotateMatrix(matrix));
+console.log(rotate(matrix)); //easier to implement and understand
