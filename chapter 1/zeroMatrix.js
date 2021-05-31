4bbbb6e3b82bf366a16b47ecbb139c12d9e799f0
hassen <hasansaid51@gmail.com>
2021-05-30 17:42:00 -0700

    better solution for zero matrix

--- chapter 1/zeroMatrix.js
+++ chapter 1/zeroMatrix.js
@@ -1,47 +1,33 @@
-//write an algo such that if an element in MxN matrix is 0, its entire row and column are set to 0
-
-function zeroMatrix(mat) {
-  let rowHash = {};
-  let colHash = {};
-
-  for (let i = 0; i < mat.length; i++) {
-    for (let j = 0; j < mat[i].length; j++) {
-      if (mat[i][j] === 0) {
-        rowHash[i] = true;
-        colHash[j] = true;
-      }
-    }
-  }
-
-  for (let i in rowHash) {
-    if (rowHash[i]) {
-      nullifyRow(mat, i);
-    }
-  }
-  for (let j in colHash) {
-    if (colHash[j]) {
-      nullifyCol(mat, j);
-    }
-  }
-  console.log(mat);
-  return mat;
-}
-
-function nullifyRow(mat, row) {
-  for (let i = 0; i < mat[0].length; i++) {
-    mat[row][i] = 0;
-  }
-}
-
-function nullifyCol(mat, col) {
-  for (let i = 0; i < mat.length; i++) {
-    mat[i][col] = 0;
-  }
-}
-
 let matrix = [
   [1, 0, 3],
   [4, 5, 6],
   [0, 8, 9],
 ];
-console.log(zeroMatrix(matrix));
+
+console.log(rotate(matrix))
+
+function rotate(mat){
+  let hash = {}
+  for(let i = 0; i < mat.length; i++){
+    for(let j = 0; j < mat.length; j++){
+      if(mat[i][j] === 0 && !hash[`${i}-${j}`]){
+        hash[`${i}-${j}`] = true
+        insertZero(mat, i, j, hash)
+      }
+    }
+  }
+  
+  return mat
+}
+
+function insertZero(mat, i, j, hash){
+  for(let k = 0; k < mat[i].length; k++){
+    mat[i][k] = 0
+    hash[`${i}-${k}`] = true
+  }
+
+  for(let l = 0; l < mat.length; l++){
+    mat[l][j] = 0
+    hash[`${l}-${j}`] = true
+  }
+}
